3561bd399f0fd3acb18b0206536814a27757291c
Nicolai Finstad Larsen <nicolaifinstadlarsen@gmail.com>
2025-01-19 13:36:09 +0100

    Using fstring print statement

--- Week-2-Coke/coke.py
+++ Week-2-Coke/coke.py
@@ -4,15 +4,15 @@ def main():
     while True:
         question = int(input("Insert coin: "))
         if question not in VALID_COIN:
-            print("Amount Due:", PRICE_LEFT)
+            print(f"Amount Due: {PRICE_LEFT}")
             continue
 
         PRICE_LEFT = PRICE_LEFT - question
         if PRICE_LEFT > 0:
-            print("Amount Due:", PRICE_LEFT)
+            print(f"Amount Due: {PRICE_LEFT}")
             continue
         else:
-            print("Change Owed:", abs(PRICE_LEFT))
+            print(f"Change Owed: {abs(PRICE_LEFT)}")
             break
 
 
