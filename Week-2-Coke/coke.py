02fc0d176c29408fe868fb77b6a8d85379357d0f
Nicolai Finstad Larsen <nicolaifinstadlarsen@gmail.com>
2025-01-19 17:02:10 +0100

    Updated coke
    Split it in to two funtions, and used return function for if statment in main

--- Week-2-Coke/coke.py
+++ Week-2-Coke/coke.py
@@ -1,40 +1,44 @@
+PRICE_LEFT = 50
+VALID_COIN = 25,10,5
+
 def main():
-    PRICE_LEFT = 50
-    VALID_COIN = 25,10,5
     while True:
         question = int(input("Insert coin: "))
+
+        #Checking that the coin inserted is acctually a coin we accept, if not we ask again
         if question not in VALID_COIN:
             print(f"Amount Due: {PRICE_LEFT}")
+            # Continue to aske question
             continue
 
-        PRICE_LEFT = PRICE_LEFT - question
+        else:
+
+            # To use two funtions i have to break after calling function here
+            if calculation(question):
+
+                 # But i only want to break if I have 0 or less left.
+                 # So I return True if i have less or == 0
+                 break
+
+
+def calculation(coin):
+        # Global variables to use them from outside scope.
+        global PRICE_LEFT
+        global VALID_COIN
+
+        PRICE_LEFT = PRICE_LEFT - coin
         if PRICE_LEFT > 0:
             print(f"Amount Due: {PRICE_LEFT}")
-            continue
+
+            # Return False to not break
+            return False
+
+        # Prints the change if we payed to much or the right amount.
         else:
             print(f"Change Owed: {abs(PRICE_LEFT)}")
-            break
 
+            # Return True to break
+            return True
 
 main()
-
-
-# def main():
-#     price_left = 50
-#     valid_coin = {25,10,5}
-
-#     while price_left > 0:
-
-#         coin = int(input("Insert coin: "))
-
-#         if coin in valid_coin:
-#             price_left -= coin
-#         else:
-#             print(f"Amount Due {price_left}")
-
-#         if price_left > 0:
-#             print(f"Amount Due: {price_left}")
-#         else:
-#             print(f"Amount Owed {abs(price_left)}")
-# main()
 
