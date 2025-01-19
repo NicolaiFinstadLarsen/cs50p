e84665b3eaf7fa40799b027ba034e2b2fd8b3c89
Nicolai Finstad Larsen <nicolaifinstadlarsen@gmail.com>
2025-01-19 13:45:28 +0100

    Made different script
    This one check is the input is in the set, and not if its not in the set.
    Seems cleaner.

--- Week-2-Coke/coke.py
+++ Week-2-Coke/coke.py
@@ -17,3 +17,24 @@ def main():
 
 
 main()
+
+
+# def main():
+#     price_left = 50
+#     valid_coin = {25,10,5}
+
+#     while price_left > 0:
+
+#         coin = int(input("Insert coin: "))
+
+#         if coin in valid_coin:
+#             price_left -= coin
+#         else:
+#             print(f"Amount Due {price_left}")
+
+#         if price_left > 0:
+#             print(f"Amount Due: {price_left}")
+#         else:
+#             print(f"Amount Owed {abs(price_left)}")
+# main()
+
