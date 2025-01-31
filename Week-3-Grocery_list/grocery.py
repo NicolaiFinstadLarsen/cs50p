332b28236ff0155648c08099a7e6618d5b405d23
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-01-31 15:07:49 +0100

    Update grocery.py

--- Week-3-Grocery_list/grocery.py
+++ Week-3-Grocery_list/grocery.py
@@ -1,4 +1,22 @@
+full_dict = {}
+
 def main():
+    while True:
+        try:
+            item = input("Item : ").strip()
+            shopping = add_to_list(item)
+        except (EOFError, KeyboardInterrupt):
+            print(shopping)
+            break
+    return
+
+def add_to_list(item):
+    global full_dict
+    if item in full_dict:
+        full_dict[item] += 1
+    else:
+        full_dict[item] = 1
+
     
 
 main()
