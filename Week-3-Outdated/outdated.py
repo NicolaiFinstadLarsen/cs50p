80ce9b8d048fa44aa1dbf9c131f0fbacdd487f2a
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-02-01 13:11:39 +0100

    Update outdated.py

--- Week-3-Outdated/outdated.py
+++ Week-3-Outdated/outdated.py
@@ -1,68 +1,75 @@
 '''
 TODO
-The printout format is the wrong way around.
+Gettin the comparison of moth and list to work.
 There is no reprompt when incorrect input is given.
 '''
+MONTHS_OF_THE_YEAR = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+    ]
 
 
 def main():
-    # full_date = input("Date: ").replace(",", "")
-    # full_date = "9/8/1988"
-    full_date = "september 8, 1988".replace(",", "")
-    print(full_date)
-    try:
-        month, day, year = full_date.strip().split("/")
-        print(month)
-    except Exception:
-        try:
-            month, day, year = full_date.split(" ")
-        except Exception as e:
-            print(f"Error as {e}")
+    full_date = input("Date: ").replace(",", "").replace("/"," ").split(" ")
+    # full_date = "9/8/1988".replace(",", "").replace("/"," ").split(" ")
+    # full_date = "september 8, 1988".replace(",", "").replace("/"," ").split(" ")
 
-    # Strictly not nessessary with two lines, but more readable to me.
+    month, day, year = full_date
+    # This was the missing piece! 
+    month = month.title()
+    # print(f"Month after the split in to 3 variables: {month}")
+
     fday, fmonth, fyear = format_date(day, month, year)
+
     print(f"{fyear}-{fmonth:02}-{fday:02}")
+
+
+def find_month_int(month):
+    global MONTHS_OF_THE_YEAR
+    for i in range(12):
+        # print(f"Value of i = {i}")
+        # print(f"Month of year index i = {MONTHS_OF_THE_YEAR[i]}")
+        # Looks like the == does not work as expected here..
+        # 
+        if month == MONTHS_OF_THE_YEAR[i]:
+            # print(f"Return value: {i+1}")
+            return i + 1
+
 
 
 
 def format_date(day, month, year):
-    months_of_the_year = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-        ]
+    '''
+    I can do some of the main function in this fuction.
+    To much is happening i main at the moment
+    '''
+    try:
+        fday = int(day)
+    # TODO reprompt for wrong input...
+    except ValueError:
+        main()
+    try:
+        fmonth = int(month)
+    except ValueError:
+        # So we do trigger the exception.
+        # Then is should be in the find_month_int function
+        # print(f"ValueError: {month}")
+        month_by_int = find_month_int(month)
+        # print(f"After function call: {month_by_int}")
+        fmonth = month_by_int
+    # fmonth is none here. What happened?
+    fyear = int(year)
 
-    if month in months_of_the_year:
-        count = 0
-        # To itterate over over the list and compare with int we need a range.
-        for i in months_of_the_year:
-            print(f"test 3 i = {i}")
-            print(f"Month is {month}")
-            # Input returns a string, so I typecast to int.
-            if i.title() == month:
-                # print(f"Test 4{i}")
-                print(f"Test 5: {year}-{count}-{day}")
-                return year, count, day
-            else:
-                count += 1
-        # print(f"Test 1")
-            return year, month, day
-    # This part is triggered if month is a number
-    else:
-        # TODO I am trying to compare the i in range of list of moths with the string month from input. Thats not how i saw it in my head.
-        # print(f"Test 2 {year}-{month}-{day}")
-        return day, month, year
-
-
-
+    return fday, fmonth, fyear
 
 main()
