3bb8bd59e7388cf3c45cff97417c36eb7ceaf9ab
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-02-03 22:56:30 +0100

    Its working now
    Some modulation possible..

--- Week-3-Outdated/outdated.py
+++ Week-3-Outdated/outdated.py
@@ -1,8 +1,13 @@
 '''
 TODO
-Gettin the comparison of moth and list to work.
-There is no reprompt when incorrect input is given.
+Design feedback from cs50 rubberduck ai:
+To modularize the input_check function while keeping the input reprompting logic, 
+you can separate the input validation and reprompting into different functions.
+For example, you could have a function that checks if the input is valid and returns a boolean value. 
+Then, in your main function, you can use a while loop to keep asking for input until the validation function returns true.
+What do you think about this approach?
 '''
+
 MONTHS_OF_THE_YEAR = [
     "January",
     "February",
@@ -20,59 +25,72 @@ MONTHS_OF_THE_YEAR = [
 
 
 def main():
-    global MONTHS_OF_THE_YEAR
-    full_date = input("Date: ").replace(",", "").replace("/"," ").split(" ") #Why not?
-    # full_date = "9/8/1988".replace(",", "").replace("/"," ").split(" ").strip() #Why not?
-    # full_date = "september 8, 1988".replace(",", "").replace("/"," ").split(" ").strip() #Why not?
-    while True:
-        try:
-            month, day, year = full_date
-            break
-        except Exception:
-                main()
-    # This was the missing piece! 
-    month = month.title()
-    
-    #print(f"Month after the split in to 3 variables: {month}")
+    day, month, year = check_input()
 
-    fday, fmonth, fyear = format_date(day, month, year)
-    if fday == None or fmonth == None or fyear == None:
-        main()
-
-
+    fday, fmonth, fyear = format_output(day, month, year)
     print(f"{fyear}-{fmonth:02}-{fday:02}")
+
+
+def check_input():
+
+    # Since we need to reprompt all of this logic needs to be in one function.
+    while True:
+
+        date = input("Date: ")
+        date = date.strip()
+
+        # Logic for date entered with / seperator
+        if ("/") in date:
+            month, day, year = date.split("/")
+            
+        # Logic for date entered with space separator and a comma
+        elif (",") in date:
+            month, day, year = date.split()
+            day = day.replace(",","")
+
+            # Global list is titlecase.
+            if month.title() in MONTHS_OF_THE_YEAR:
+                month = find_month_int(month)
+                
+            else: 
+                continue
+
+        # All other formats results in reprompt from this line or line 45
+        else:
+            continue
+        
+        # We could assume all month had 31 days.
+        try:
+
+            if int(day) > 31 or int(month) > 12:
+                continue
+            else:
+                break
+        # Value error can be raised if month is placed in the wrong index of input string.
+        except ValueError:
+            continue
+
+    return day, month, year
+
+def format_output(day, month, year):
+
+    # Formating done to print f string with 2 digits for month and day.
+    day = int(day)
+    month = int(month)
+    year = int(year)
+
+    return day, month, year
 
 
 def find_month_int(month):
     global MONTHS_OF_THE_YEAR
-    for i in range(12):
-        # print(f"Value of i = {i}")
-        # print(f"Month of year index i = {MONTHS_OF_THE_YEAR[i]}")
-        # Looks like the == does not work as expected here..
-        # 
-        if month == MONTHS_OF_THE_YEAR[i]:
-            print(f"Return value: {i+1}")
-            return i + 1
+
+    month = MONTHS_OF_THE_YEAR.index(month) 
+
+    # Adding 1 makes sure we account for 0 indexed list
+    return month +1 
 
 
-def format_date(day, month, year):
-    print(f"Format_date day: {day}")
-    print(f"Format_date month: {month}")
-    try:
-        fday = int(day)
-    except ValueError:
-        return
-    try:
-        fmonth = int(month)
-    except ValueError:
-        # So we do trigger the exception.
-        # Then is should be in the find_month_int function
-        # print(f"ValueError: {month}")
-        month_by_int = find_month_int(month)
-        # print(f"After function call: {month_by_int}")
-        fmonth = month_by_int
-    fyear = int(year)
-
-    return fday, fmonth, fyear
-
-main()
+# Probably not going to be a public lib, but good practice.
+if __name__ == "__main__":
+    main()
