7c58256d00bbdc5e308b82167af0b9110d952875
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-02-08 00:02:01 +0100

    Update professor.py

--- Week-4-Little_professor/professor.py
+++ Week-4-Little_professor/professor.py
@@ -1,30 +1,55 @@
+'''
+TODO
+Code cleanup, alot of uneeded code right now.
+Get closer to pset spec with:¨
+Making 10 questions in a list right away mabye?
+Remove ValueError checks in get_level?
+Raise ValueError if level is not correct in gen int func instead?
+'''
+
 import random
 import sys
 
+correct_count = 0
+level = 0
+ROUNDS_OF_QUESTION = 10
 
 def main():
-    count = 0
+    global correct_count
+    global ROUNDS_OF_QUESTION
     error_count = 0
+    
     x,y=get_level()
+
     while True:
         try:
             answer = int(input("Answer: "))
         except ValueError:
             continue
+
         # This whole wrong answer check should be in a separate func or loop so i can do three checks before moving on
-        if answer != x+y:
-            print("EEE")
-            answer = int(input("Answer: "))
-            error_count +=1
-            continue
-        if error_count == 3:
-            sys.exit(0)
-        elif answer == x+y:
-            print("correct")
+        while True:
+            if answer != x+y:
+                print("EEE")
+                error_count +=1
+                # print(f"Error count: {error_count}")
+                if error_count == 3:
+                    print(f"{x} + {y} = {x+y}")
+                    sys.exit(0)
+                
+                # print(f"Error count {error_count}")
+                answer = int(input("Answer: "))
+
+            elif answer == x+y:
+                # print("correct")
+                correct_count += 1
+                if correct_count == ROUNDS_OF_QUESTION:
+                    print(f"Score {correct_count-error_count}")
+                break
 
         # Count to 3 to make it do next
         # Also do 10 qestions at a time
-        if count < 10:
+        if correct_count < ROUNDS_OF_QUESTION:
             x,y = get_level()
             continue
         else:
@@ -32,11 +57,15 @@ def main():
             
 
 def get_level():
+    global correct_count
+    global level
+
     while True:
-        try:
-            level = int(input("Level: "))
-        except ValueError:
-            continue
+        if correct_count == 0:
+            try:
+                level = int(input("Level: "))
+            except ValueError:
+                continue
 
         if level in range(1,4):
             x, y = generate_integer(level)
