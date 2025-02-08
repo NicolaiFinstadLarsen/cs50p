629e3981576965a03313c32951d7dd47c5a3cac9
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-02-08 11:45:28 +0100

    Update professor.py
    I dont get the check50 list req

--- Week-4-Little_professor/professor.py
+++ Week-4-Little_professor/professor.py
@@ -1,67 +1,64 @@
-'''
-TODO
-Code cleanup, alot of uneeded code right now.
-Get closer to pset spec with:¨
-Making 10 questions in a list right away mabye?
-Remove ValueError checks in get_level?
-Raise ValueError if level is not correct in gen int func instead?
-'''
-
 import random
-import sys
 
-correct_count = 0
-level = 0
 ROUNDS_OF_QUESTION = 10
 CORRECT_LEVELS = [1,2,3]
 
 def main():
     formated_level = get_level()
-    generate_integer(formated_level)
+    numbers = generate_integer(formated_level)
+    score = run_answer(numbers)
+    print(score)
 
 
 def get_level():
     while True:
         try:
             level = int(input("Level: "))
-            if level not in CORRECT_LEVELS:
-                continue
-            else:
-                return level
+            if level in CORRECT_LEVELS:
+                return level    
         except ValueError:
             continue
 
 
 def generate_integer(level):
-    global correct_count
-    while True:
+    # This is so ugly, but since check50 wants the questions in a list. This is how i managed to do that.
+    # My code without a list is much prettier.
+    # Im also proud over my order of magnitude solution to the n digits!
+    question = [(random.randint(10**(level-1), 10**level - 1),
+                 random.randint(10**(level-1), 10**level - 1)) 
+                 for i in range(ROUNDS_OF_QUESTION)]
+    
+    return question
+
+
+def run_answer(numbers):
+
+    # I look like i need to have this in another func when running check 50...
+    correct_count = 0
+
+    for x, y in numbers:
         tries = 0
-        for i in range(ROUNDS_OF_QUESTION):
-            x = random.randint(10**level/10, 10**level-1)
-            y = random.randint(10**level/10, 10**level-1)
-            print(f"{x} + {y} = ")
-            while True:
-                try:
-                    answer = int(input())
-                except ValueError:
-                    continue
+        while True:
+            try:
+                answer = int(input(f"{x} + {y} = "))
                 if answer == x+y:
                     correct_count += 1
                     break
                 else:
                     print("EEE")
-                    tries += 1
-                if tries == 3:
-                    print(f"{x} + {y} = {x+y}")
-                    sys.exit()
-                else:
-                    continue
-        print(f"Score: {correct_count-tries}")
-        break
-                
+                    
+            except ValueError:
+                continue
 
+            tries += 1
 
+            if tries == 3:
+                print(f"{x} + {y} = {x+y}")
+                break
+            else:
+                continue
 
+    return correct_count
 
 
     
