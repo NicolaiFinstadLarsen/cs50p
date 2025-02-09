f2332982c5b5649feb656b23ff9e9de37853aa58
Nicolai Finstad Larsen <nicolaifinstadlarsen@gmail.com>
2025-02-09 09:02:12 +0100

    pset 6 week 5

---
+++ Week-4-Bitcoin/API_test.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+def main():
+    
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    content = response.json()
+    # This to prettyprint so I can understand what I am looking for.
+    # print(json.dumps(content, indent=2))
+
+    price = (content["bpi"]["USD"]["rate"])
+    price = float(price.replace(",",""))
+    # price = float(price)
+    # price = f"{price:,d}"
+    print(f"{price:,.4f}")
+
+
+main()
--- Week-4-Bitcoin/bitcoin.py
+++ Week-4-Bitcoin/bitcoin.py
@@ -9,21 +9,36 @@ def main():
     else:
         try:
             x = float(sys.argv[1])
-            price = get_price(x)
+            # print(type(x))
         except ValueError:
             sys.exit(2)
-
-    if price:
-        print(f"{price:,}")
-    else:
-        sys.exit(3)
+    price = get_price(x)
+    # if price:
+    print(price)
+    # else:
+        # sys.exit(3)
 
 def get_price(n):
-    '''
-    get API call
-    '''
-    return 100_000 * n
-        
-    
 
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    content = response.json()
+    
+    # This to prettyprint so I can understand what I am looking for.
+    # print(json.dumps(content, indent=2))
+
+    price = (content["bpi"]["USD"]["rate"])
+    price = price.replace(",","")
+    price = float(price)
+    format_price = f"{price:,.4f}"
+
+    # Might need to return this variable and format later 
+    
+    print(price)
+
+    # return format_price * n
+        
+'''
+To check error code in powershell use:
+echo $LASTERRORCODE
+'''    
 main()
