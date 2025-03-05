c5d91eaa2e1d1622a86b39d01dcce9cfee7ecf9e
Nicolai Finstad Larsen <nicolaifinstadlarsen@gmail.com>
2025-03-05 08:58:36 +0100

    pset 1 week 6

--- Week-6-Counting_lines/lines.py
+++ Week-6-Counting_lines/lines.py
@@ -1,6 +1,7 @@
 import sys
 
 def main():
+    # Exit messages as per problem spec's
     if len(sys.argv) < 2:
         # print("Usage python lines.py 'file to read'")
         sys.exit("Too few command-line arguments")
@@ -13,15 +14,24 @@ def main():
     except FileNotFoundError:
         sys.exit("File does not exist")
 
+
+'''
+Reading lines in a .py file 
+and not counting comment # lines or whitespacelines
+'''
+
 def read(name):
     counter = 0
     with open(name, "r") as file:
         rows = file.readlines() # readlines and not readline.
 
         for row in rows:
-            row.lstrip()
-            if row.startswith("#") or row.startswith("\n"):
-                pass
+            row = row.lstrip() 
+            # Since row.strip() == True is there is text left after strip \n
+            # not row.strip to get a False if the row has text. \n
+            # To not skip valid code lines.  
+            if row.startswith("#") or not row.strip():  # not row.strip to get a true of the row is not empty. 
+                continue
             else:
                 # print(row)
                 counter += 1
