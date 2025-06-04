01fc08dae0ff5e631c3274b1e4e343bdf0b57c8a
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-06-04 13:24:29 +0200

    raise error and __str__
    Error raising and __str__ method for classes

--- Week-8-my_stuff/student_class_3.py
+++ Week-8-my_stuff/student_class_3.py
@@ -1,12 +1,19 @@
 class Student:
     def __init__(self, name, house):
+        if not name: # We can raise errors in the class method to validate the data passed in.
+            raise ValueError("Not working")
+        if house not in ["oslo", "bergen", "trondheim"]:
+            raise ValueError("Not a city")
         self.name = name
         self.house = house
+
+    def __str__(self): # a method that is called when a string is needed from class
+        return f"{self.name} from {self.house}"
 
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student) # Printing the class, needs a string
 
 
 def get_student():
