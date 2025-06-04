a611de463180a38f5e495d7e9bf1c9da88bd9e9b
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-06-04 15:10:19 +0200

    Update student_class_4.py
    easy custom

--- Week-8-my_stuff/student_class_4.py
+++ Week-8-my_stuff/student_class_4.py
@@ -1,25 +1,39 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name: 
             raise ValueError("Not working")
         if house not in ["oslo", "bergen", "trondheim"]:
             raise ValueError("Not a city")
         self.name = name
         self.house = house
+        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {self.house}"
+    
+    def charm(self):
+        match self.patronus:
+            case "stag":
+                return "😄"
+            case "otter":
+                return "🦦"
+            case "jack russel terrier":
+                return "🐶"
+            case _:
+                return "🪄"
 
 
 def main():
     student = get_student()
-    print(student)
+    print("Expecto Patronum!")
+    print(student.charm())
 
 
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return Student(name, house)
+    name = input("Name: ").lower()
+    house = input("House: ").lower()
+    patronus = input("Patronus: ").lower()
+    return Student(name, house, patronus)
 
 
 if __name__ == "__main__":
