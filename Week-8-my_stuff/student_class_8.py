74c33e40d897eadeab8b88f0e4cd32a11bd16b07
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-06-04 16:40:21 +0200

    Update student_class_8.py

---
+++ Week-8-my_stuff/student_class_8.py
@@ -0,0 +1,30 @@
+class Wizard:
+    def __init__(self, name):
+        if not name:
+            raise ValueError("No name")
+        self.name = name
+
+    ...
+
+    
+class Student(Wizard): # Dette gir oss inheritence fra Wizard class
+    def __init__(self, name, house):
+        super().__init__(name) # Super er foreldre classen og init name \n
+        # er for å bruke name fra den klassen
+        # self.name = name # Derfor trenger vi ikke denne.
+        self.house = house
+
+    ...
+
+
+class Professor(Wizard): 
+    def __init__(self, name, subject):
+        super().__init__(name)
+        # self.name = name
+        self.subject = subject
+
+    ...
+
+wizard = Wizard("Albus")
+student = Student("Harry", "Gryffindor")
+professor = Professor("Sverus", "Defense Against The Black Arts")
