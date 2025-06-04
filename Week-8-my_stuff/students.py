f570803cd76da81cda5e06a2fce9076084da741a
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-06-04 12:24:06 +0200

    Tuple and list muteability

--- Week-8-my_stuff/students.py
+++ Week-8-my_stuff/students.py
@@ -1,12 +1,14 @@
 def main():
     student = get_student()
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw" # Changing student house
     print(f"{student[0]} from {student[1]}")
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return name, house
+    return [name, house] # Returns a list, not a tuple. Since tuple is inmuteable. List is muteable.
 
 
 if __name__ == "__main__":
