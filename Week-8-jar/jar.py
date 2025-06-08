c94cb5055d006ecde67dbe95e8cbed2e1760d3be
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-06-08 12:01:09 +0200

    update halfway there

--- Week-8-jar/jar.py
+++ Week-8-jar/jar.py
@@ -1,15 +1,24 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
+        if self.capacity < 0:
+            raise ValueError
+        self.total = 0
+        self.capacity = capacity
+
 
     def __str__(self):
-        ...
+        return f"{self.total * '🍪'}"
 
     def deposit(self, n):
-        ...
+        if self.total + n > self.capacity:
+            raise ValueError
+        self.total += n # no need to return, just update state.
+        
 
     def withdraw(self, n):
-        ...
+        if self.total - n < 0:
+            raise ValueError
+        self.total -= n
 
     @property
     def capacity(self):
