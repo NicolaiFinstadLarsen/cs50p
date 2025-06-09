852a480506e09d50df438b4c768aeaf9143e6ccf
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-06-09 07:46:00 +0200

    Functions layed out
    I think

--- Week-9-project/project.py
+++ Week-9-project/project.py
@@ -1,6 +1,40 @@
 import pandas as pd
+def main():
+    read_file = "ENQ2023Z_1min.csv"
+    df = format_df(read_file)
+    df = calculate_sma_window(df)
+    visualisation(df)
+    
 
-df = pd.read_csv("ENQ2023Z_1min.csv", names=["Contract", "Date", "Time", "Open", 
-                                             "High", "Low", "Close", "Volume", 
-                                             "Something"])
-print(df.head(3))
+
+def format_df(file):
+    df = pd.read_csv(file, names=["Contract", "Date", "Time", "Open", 
+                                                "High", "Low", "Close", "No_need", 
+                                                "Volume"])
+    df = df.drop(columns="No_need")
+    return df
+
+
+def calculate_sma_window(data):
+    ...
+
+
+def get_signal():
+    ...
+
+def take_position():
+    ...
+
+
+def account_equity():
+    ...
+
+
+def visualisation(df):
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
+
+
